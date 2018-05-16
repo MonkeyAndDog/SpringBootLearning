02c7ecb287b355e4d86008162b59c334e21ae259
Monkey <1466947023@qq.com>
2018-05-16 18:37:17 +0800

    参数验证学习完成。

--- README.md
+++ README.md
@@ -299,10 +299,57 @@ public class FileSystemStorageService implements StorageService {
 }
 ```
 
+参数验证
+---
+使用以下依赖进行处理（Spring使用hibernate的验证器，该项目用的是javax的验证器接口）
+```xml
+<dependency>
+    <groupId>org.hibernate.validator</groupId>
+    <artifactId>hibernate-validator</artifactId>
+</dependency>
+```
+在domain类中需要验证的字段上加注解。
 
+例如：
+```java
+package hello;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
+public class PersonForm {
 
+    @Min(18)
+    private int age;
 
+    @NotNull
+    @Size(max = 20, min = 6)
+    private String name;
 
+    public int getAge() {
+        return age;
+    }
 
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonForm{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
+```
+然后在`BindingResult`接口中获取相应的错误内容。
--- src/main/java/hello/MainController.java
+++ src/main/java/hello/MainController.java
@@ -1,18 +1,24 @@
 package hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/demo")
 public class MainController {
 
     private UserRepository userRepository;
+    private final Logger logger = LoggerFactory.getLogger(MainController.class);
 
-    @RequestMapping("/add")
+    @RequestMapping("/add/user")
     public @ResponseBody String addNewUser(@RequestParam String name, @RequestParam String email) {
         User user = new User();
         user.setEmail(email);
@@ -33,5 +39,16 @@ public class MainController {
     @Autowired
     public void setUserRepository(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    @RequestMapping("/add/person")
+    @ResponseBody
+    public String addPerson(@Valid PersonForm personForm, BindingResult result) {
+        if (result.hasErrors()) {
+            logger.info(result.toString());
+            return "参数非法";
+        }
+        logger.info(personForm.toString());
+        return personForm.toString();
     }
 }
